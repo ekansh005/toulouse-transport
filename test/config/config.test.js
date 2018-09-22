374601f3856d84d8c08978b1d6ba1b6da509bd80
Ekansh Saxena <ekansh005@gmail.com>
2018-09-23 01:35:52 +0200

    before installing rewire

---
+++ test/config/config.test.js
@@ -0,0 +1,24 @@
+require('./../../config/config');
+const expect = require('expect');
+
+describe('Enviornment Variable Configs', () => {
+  it('should set NODE_ENV to test', (done) => {
+    expect(process.env.NODE_ENV).toBe('test');
+    done();
+  });
+
+  it('should have PORT as 3000', (done) => {
+    expect(Number(process.env.PORT)).toBe(3000);
+    done();
+  });
+
+  it('should have API key', (done) => {
+    expect(process.env.TISSEO_API_KEY).toBeTruthy();
+    done();
+  });
+
+  it('should have Base URL', (done) => {
+    expect(process.env.TISSEO_URL).toBeTruthy();
+    done();
+  });
+});
